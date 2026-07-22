0c4d21028954d9b880090820d33fc4a7367d81c4
Ryu <112185174+pw56@users.noreply.github.com>
2026-07-22 17:22:02 +0900

    型の再エクスポートの構文ミスを修正

--- listings/getGroups/types.ts
+++ listings/getGroups/types.ts
@@ -4,4 +4,4 @@ type Group = Detection[];
 type Groups = Group[];
 
 export type { Group, Groups };
-export { Detection, BoundingBox } from '@mediapipe/tasks-vision';
+export { type Detection, type BoundingBox } from '@mediapipe/tasks-vision';
