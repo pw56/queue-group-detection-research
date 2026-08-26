5463ca641f9a6bc0eec3f687b46c4d8b467fee2c
Ryu <112185174+pw56@users.noreply.github.com>
2026-08-26 18:14:03 +0900

    パスのルートの指定ミスを修正

--- src/approach_10_change_detection_model/vite.config.ts
+++ src/approach_10_change_detection_model/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/src/approach_10_change_detection_model/dist/',
+  base: '/queue-group-detection-research/src/approach_10_change_detection_model/dist/',
   plugins: [
     tailwindcss(),
   ]
