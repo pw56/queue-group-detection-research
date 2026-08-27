fadccaa0e2f43708234c1969bb5594be13c594d5
Ryu <112185174+pw56@users.noreply.github.com>
2026-08-27 10:39:23 +0000

    Viteの設定ファイルの書き方を簡略化

--- src/approach_10_change_detection_model/vite.config.ts
+++ src/approach_10_change_detection_model/vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig(({}) => {
-  return {
-    base: './',
-    plugins: [
-      tailwindcss(),
-    ]
-  };
+export default defineConfig({
+  base: './',
+  plugins: [
+    tailwindcss(),
+  ]
 });
